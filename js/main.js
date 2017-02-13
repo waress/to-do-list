f1982f99137fb91a58d7841500ec970743d1aaf2
Varga Zoltán <waress76@gmail.com>
2017-02-13 21:35:23 +0100

    Add and set list, list items styles; JS click event to add button

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/* User clicked on the add button */
+/* If there is any text inside the item  field, add that text to the todo list */
+document.getElementById('add').addEventListener('click', function() {
+  var value = document.getElementById('item').value;
+  if (value) {
+    /* console.log('There was a value, wooh!'); */
+  }
+});
