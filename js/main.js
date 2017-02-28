739b752737a38d4de11ad81c2dbc49a6f99ca69a
Varga Zoltán <waress76@gmail.com>
2017-02-28 19:31:54 +0100

    set remove item event

--- js/main.js
+++ js/main.js
@@ -12,6 +12,13 @@ document.getElementById('add').addEventListener('click', function() {
   }
 });
 
+function removeItem(){
+  var item = this.parentNode.parentNode;
+  var parent = item.parentNode;
+
+  parent.removeChild(item);
+}
+
 // Adds a new item to the todo list
 function addItemTodo(text) {
   var list = document.getElementById('todo');
@@ -25,6 +32,9 @@ function addItemTodo(text) {
   var remove = document.createElement('button');
   remove.classList.add('remove');
   remove.innerHTML = removeSVG;
+
+  // Add click event for removing item
+  remove.addEventListener('click', removeItem);
 
   var complete = document.createElement('button');
   complete.classList.add('complete');
